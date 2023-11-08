ef6cabb7594861c90ba7aa8dad87fe494eae0ed1
Krzysztof Laudanski <laudanski.krzysztof86@gmail.com>
2023-11-08 15:53:03 +0100

    delete useless test

--- utils/formatFullname.js
+++ utils/formatFullname.js
@@ -2,7 +2,6 @@ module.exports = (fullName) => {
   if (typeof fullName !== 'string') return 'Error';
   if (!(/^[A-Za-z\s]+$/.test(fullName))) return 'Error';
   if (fullName.split(' ').length !== 2) return 'Error';
-  if (fullName.length < 2) return 'Error';
   const [firstName, lastName] = fullName.split(' ');
   if (!firstName || !lastName) return false;
   if (firstName.length < 2 || lastName.length < 2) return 'Error';
